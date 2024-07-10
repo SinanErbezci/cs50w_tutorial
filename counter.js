aba81536911dcf4ccbf057ee777e6b0477de5b78
SinanErbezci <sinanerbezci@hotmail.com>
2024-07-10 15:05:49 +0300

    new stuff

--- counter.js
+++ counter.js
@@ -1,4 +1,8 @@
-let counter = 0;
+// storing information on browser with localStrorage
+if (!localStorage.getItem('counter')) { // if there is not counter variable
+    localStorage.setItem('counter', 0);
+}
+
 function hello() {
     const heading = document.querySelector('h1'); // if value never gonna change. use const
     if (heading.innerHTML === 'Hello') { // === -> strict equality. has to be same type
@@ -10,14 +14,20 @@ function hello() {
 }
 function count() {
     const heading = document.querySelector('h2');
+    let counter = localStorage.getItem('counter');
     counter++;
     heading.innerHTML = counter;
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`); // for f strings use `<string>` ${<variable>}
-    }
+    localStorage.setItem('counter', counter);
+
+    // if (counter % 10 === 0) {
+    //    alert(`Count is now ${counter}`); // for f strings use `<string>` ${<variable>}
+    // }
 }
 // If you do this before DOM is loaded, then js cant find the elements.
 document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('h2').innerHTML = localStorage.getItem('counter');
     document.getElementById('count').onclick = count;
-    document.getElementById('hello').onclick = hello;    
+    document.getElementById('hello').onclick = hello;
+
+    setInterval(count, 1000); // every 1000ms runs the count function
 }); // addeventlistener takes two arguments: event and the function 
